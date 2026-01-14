e3445d231377b4cc5b8bbc85b0eb3e9fcb6e3629
MarmoPL <77023982+MarmoPL@users.noreply.github.com>
2026-01-14 12:42:42 +0100

    Update filters.py with only_positive and only_even functions

--- analyzer/filters.py
+++ analyzer/filters.py
@@ -1,35 +1,13 @@
-"""
-Moduł filters - funkcje filtrujące dla pakietu analyzer.
-"""
-
 from typing import Iterator, Iterable
 
 
 def only_positive(data: Iterable[float]) -> Iterator[float]:
-    """
-    Filtruje dane, zwracając tylko liczby dodatnie.
-    
-    Args:
-        data: Iterable z liczbami.
-        
-    Yields:
-        Liczby większe od 0.
-    """
     for value in data:
         if value > 0:
             yield value
 
 
 def only_even(data: Iterable[float]) -> Iterator[float]:
-    """
-    Filtruje dane, zwracając tylko liczby parzyste.
-    
-    Args:
-        data: Iterable z liczbami.
-        
-    Yields:
-        Liczby parzyste (podzielne przez 2).
-    """
     for value in data:
         if value % 2 == 0:
             yield value
