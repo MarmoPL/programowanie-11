6e48ed8cddebbfb16954b519079f3abbfbc3ae3f
MarmoPL <77023982+MarmoPL@users.noreply.github.com>
2026-01-14 12:42:32 +0100

    Update analyzer/__init__.py with new imports and exports

--- analyzer/__init__.py
+++ analyzer/__init__.py
@@ -1,12 +1,3 @@
-"""
-Pakiet analyzer - narzędzia do analizy danych liczbowych.
-
-Eksportuje główne funkcje z modułów:
-- read_numbers z io_utils
-- mean, min_max z stats
-- only_positive, only_even z filters
-"""
-
 from analyzer.io_utils import read_numbers
 from analyzer.stats import mean, min_max
 from analyzer.filters import only_positive, only_even
